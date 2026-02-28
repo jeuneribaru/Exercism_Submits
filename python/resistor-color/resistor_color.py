fcc60cd8dcbb3d80c5ecf7e6a2a1221b8d969c30
jeuneribaru <ton-email@example.com>
2026-02-28 15:31:22 +0100

    Submit Resistor Color

---
+++ python/resistor-color/resistor_color.py
@@ -0,0 +1,26 @@
+dic = {
+    "black": 0,
+    "brown": 1,
+    "red": 2,
+    "orange": 3,
+    "yellow": 4,
+    "green": 5,
+    "blue": 6,
+    "violet": 7,
+    "grey": 8,
+    "white": 9,
+    }
+
+def color_code(color):
+    return dic[color]
+
+
+def colors():
+    l = []
+    for i in dic:
+        l.append(i)
+    return l
+
+
+print(color_code("violet"))
+print(colors())

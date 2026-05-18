5f56e19b9d9ce42e81fbb0548ff7ef296bbbc58b
jeuneribaru <julienpons007@gmail.com>
2026-05-18 16:37:49 +0200

    Submit Resistor Color C

---
+++ c/resistor-color/resistor_color.c
@@ -0,0 +1,14 @@
+#include "resistor_color.h"
+
+int color_code (resistor_band_t COLOR) {
+    return COLOR ;
+}
+
+const resistor_band_t *colors(void)
+{
+    static const resistor_band_t all_colors[] = {
+        BLACK, BROWN, RED, ORANGE, YELLOW,
+        GREEN, BLUE, VIOLET, GREY, WHITE
+    };
+    return all_colors ;
+}
